623ac9de783d24e75840d488990c79f4cfc40817
neo knight <raunakhebballi@gmail.com>
2020-08-15 11:58:56 +0530

    first commit

---
+++ frontend/build/precache-manifest.156f1a91ea05de8f228b815866a6ed1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f835aa72ff19000ae7d01d6c877b0f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b0cce977d4c37ffa2b6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9fffc4880d5a2102faff",
+    "url": "/static/js/2.69dccf0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69dccf0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0cce977d4c37ffa2b6",
+    "url": "/static/js/main.c917969f.chunk.js"
+  },
+  {
+    "revision": "286a80014f97039f5024",
+    "url": "/static/js/runtime-main.8f5b2370.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ frontend/src/game/solo/Solo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Solo.css';
+
+const Solo = () => {
+    return (
+        <h5>im goin in alone</h5>
+    );
+};
+
+export default Solo;
