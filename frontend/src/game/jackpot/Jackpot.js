182a2e9c658a096feb3d054ce197845299e21a9f
neo knight <raunakhebballi@gmail.com>
2020-08-17 13:57:06 +0530

    neo config partial done

--- frontend/src/game/jackpot/Jackpot.js
+++ frontend/src/game/jackpot/Jackpot.js
@@ -1,29 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Jackpot.css'
 
 import Gamer from '../../shared/components/Gamer'
 
 const Jackpot = (props) => {
 
-    let AllPlayerNames = []
-    props.all_players.map(player => {
-        AllPlayerNames.push(player.name)
-    })
-    let roundSkoresList = []
-    props.all_players.map(player => {
-        roundSkoresList.push({id: player.id, skore: 0})
-    })
-
     const [ gang, setGang ] = useState(false)
     const [ stakes, setStakes ] = useState(100)
     const [ config, setConfig ] = useState(true)
     const [ gameRound, setGameRound ] = useState(1)
     const [ gameState, setGameState ] = useState(props.all_players)
-    const [ playing, setPlaying ] = useState(AllPlayerNames)
-    const [ roundSkores, setRoundSkores ] = useState([roundSkoresList])
+    const [ playing, setPlaying ] = useState(null)
+    const [ getPlayers, setGetPlayers ] = useState(null)
+    const [ nextId, setNextId ] = useState('p'+String(1))
 
+    const newPlayerNameInputRef = useRef(null)
 
-    const gangFlagHandler = () => {
+    const gangFlagToggleHandler = () => {
         if (gang === true){
             setGang(false)
         }
@@ -36,55 +29,38 @@ const Jackpot = (props) => {
         setStakes(e.target.value)
     }
 
-    const beginJackpotHandler = (e) => {
+    const newGameGenerateHandler = (e) => {
+        e.preventDefault()
+        // TAKE THE PLAYING ARRAY AND GENERATE THE TABLE FOR THEM
+        // EACH SUBMIT FROM TABLE SEND TO BACK END
         setConfig(false)
-        if(gang === true){
-            //all 5 render
-            /*
-            props.all_players.map(player => {
-                //console.log(player.name)
-            })
-            */
-           setPlaying(AllPlayerNames)
-           //console.log(AllPlayerNames)
+
+      
+    }
+
+    const addPlayerHandler = (e) => {
+        e.preventDefault()
+        let currentPlaying = []
+        if(playing !== null){
+            currentPlaying = [...playing]
         }
         else{
-            let tempArray = []
-            let newPlayingState = []
-            //render only the first 3 players from jackpot
-            props.all_players.map(player => {
-                tempArray.push(player.name)
-            })
-            let x = 0
-            for(x; x<3; x++){
-                //console.log(tempArray[x])
-                newPlayingState.push(tempArray[x])
-
-            }
-            setPlaying(newPlayingState)
+            currentPlaying = []
         }
-    }
+        currentPlaying.push(newPlayerNameInputRef.current.value)
+        console.log(currentPlaying)
+        setPlaying(currentPlaying)
+        newPlayerNameInputRef.current.value = ''
 
-    const addToSkoreHandler = (e, id) => {
-        const playerIndex = gameState.findIndex(p => {
-            return p.id === id
-        })
-        const player = {
-            ...gameState[playerIndex]
-        }
-        //let originalSkore = player.skore
-        //originalSkore = originalSkore + e.target.value
-        const players = [...gameState]
-        players[playerIndex] = player 
-        console.log(player)
-        console.log(id)
-        console.log(e.target.value)
-        setGameState(players)
     }
+    
 
     const onRoundSubmitHandler = (e) => {
         e.preventDefault()
-        
+        //send http post req 
+        //expect an array of skores to be returned
+        // for all playing map skores in state here
+        // check out redux once properly maybe it can help
         let currentRound = gameRound + 1
         setGameRound(currentRound)
         let arr_skore_input = document.getElementsByClassName("current_skore_input")
@@ -110,8 +86,9 @@ const Jackpot = (props) => {
         //console.log(gameState)
     }
 
-
-
+    const configToggleHandler = () => {
+        setConfig(false)
+    }
 
 
 
@@ -131,15 +108,40 @@ const Jackpot = (props) => {
                 <br />
                 <label><b>gang flag: </b></label><label style={style}><b>{gang ? "ON" : "OFF" }</b></label>
                 {gang ?
-                <button value="reset" onClick={gangFlagHandler} className="game_flag_button">reset</button>
+                <button value="reset" onClick={gangFlagToggleHandler} className="game_flag_button">reset</button>
                 :
-                <button value="set" onClick={gangFlagHandler} className="game_flag_button" >set</button>
+                <button value="set" onClick={gangFlagToggleHandler} className="game_flag_button" >set</button>
                 }
-                <button className="game-button" onClick={beginJackpotHandler}>START</button>
-            </div>
-         
+                {!gang ?
+                <div className="new_game_form_div">
+                    <label>enter player name to add </label><input ref={newPlayerNameInputRef} type='text' className="game-input1"></input>
+                    <button className="game-button" onClick={addPlayerHandler}>ADD</button>
+                    <button className="game-button" onClick={newGameGenerateHandler}>START</button>
+                </div>
+                
+                :
+                null
+                }
+                {playing === null ?
+                    null 
+                    :
+                    
+                    playing.map((p, index) => {
+                        return (
+                            <span style={style}>{p} </span>
+                        )
+                    })
+
+                }
+                {!gang ?
+                null
+                :
+                <button className="game-button" onClick={configToggleHandler}>START</button>
+                }    
+            </div> 
         )
     }
+
     else{
         if(gang){
             const housefull = {
@@ -178,7 +180,6 @@ const Jackpot = (props) => {
         }
         else{
             return (
-                //usual team block render
                 <div className="game-container">
                     <div className="game">
                     <h3>Jackpot {stakes}</h3>
@@ -186,22 +187,19 @@ const Jackpot = (props) => {
                         <div className="game-container">
                         <form onSubmit={onRoundSubmitHandler}>
                             {
-                                props.all_players.map((player, index) => {
-                                    if (index < 3){
-                                        //render the player
-                                        return (
-                                            <Gamer 
-                                            key={player.id}
-                                            id={player.id} 
-                                            name={player.name} 
-                                            skore={gameState[index].skore}
-                                            />
-                                        )
-                                    }
+                                playing.map((player, index) => {
+                                    return (
+                                        <Gamer 
+                                        key={player+index}
+                                        id={player+index} 
+                                        name={player} 
+                                        skore={0} 
+                                        />
+                                    )
                                 })
                             }
                             <button className="game-button" type="submit">NEXT</button>
-                            </form>
+                        </form>
                             
                         </div>
                         
